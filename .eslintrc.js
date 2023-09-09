ba2803aa056f276b9233bfe38ec710c7911d6e68
Pedro Souza <pedrops244@hotmail.com>
2023-09-09 13:50:45 -0300

    create to-do-list with react

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+    'react/forbid-prop-types': 0,
+    'no-unused-vars': 0,
+    'import/no-duplicates': 0,
+    'class-methods-use-this': 0,
+    'import/no-extraneous-dependencies': 0,
+  },
+};
